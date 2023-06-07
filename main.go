e0a53b6fd70ad9cfeabbaf0e648d4fb3a0f066c0
Anis <anis.rahman@nfq.com>
2023-06-07 12:39:16 +0700

    Postgresql setup with gorm

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func init() {
@@ -15,11 +18,34 @@ func init() {
 }
 
 func main() {
+	//postgresql setup
+	appEnv, err := godotenv.Read()
+	if err != nil {
+		log.Fatal("Error reading .env")
+	}
+	DB_HOST := appEnv["DB_HOST"]
+	DB_USER := appEnv["DB_USER"]
+	DB_PASSWORD := appEnv["DB_PASSWORD"]
+	DB_NAME := appEnv["DB_NAME"]
+	DB_PORT := appEnv["DB_PORT"]
+	DB_SSLMODE := appEnv["DB_SSLMODE"]
+	DB_TIMEZONE := appEnv["DB_TIMEZONE"]
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=%s", DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, DB_SSLMODE, DB_TIMEZONE)
+
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	fmt.Println("database connection successful!")
+	fmt.Printf(db.Name())
+	fmt.Println()
+	if err != nil {
+		log.Fatal("Error connection to database")
+	}
+	//gin router setup
 	router := gin.Default()
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "App is running",
+			"connStr": connStr,
 		})
 	})
 
